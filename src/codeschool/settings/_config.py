b1d109cf8e45c1f6b203675d635f61574c8904a8
ArthurTemporim <arthurrtl@gmail.com>
2017-09-11 07:50:25 -0300

    Solve merge problems.

--- src/codeschool/settings/_config.py
+++ src/codeschool/settings/_config.py
@@ -195,7 +195,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',

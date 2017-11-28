218c2acc070d37c1ba6acc57114d06f373f10015
Vinícius Oliveira <vinifladf@gmail.com>
2017-11-28 18:16:27 -0200

    Fix tests of field
    Signed-off-by: Vinícius Oliveira <vinifladf@gmail.com>
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>

--- src/codeschool/lms/fields/tests/test_field.py
+++ src/codeschool/lms/fields/tests/test_field.py
@@ -1,24 +1,26 @@
 import pytest
 
-from codeschool.lms.fields.models import Field, FieldValue
+from codeschool.lms.fields.models import (Field, FieldValue, get_form_class, get_form_field)
 from codeschool.core.users.factories import (FullUserFactory, UserFactory, make_students)
 
 
 @pytest.fixture
 def char_field():
     name = "github"
-    field_type = 1
+    field_type = Field.TYPE_URL
     description = "Insira aqui seu GitHub"
-    return Field(name=name, field_type=field_type, description=description)
+    return Field(id=0, name=name, field_type=field_type, description=description)
+
 
 @pytest.fixture
 def db_char_field(char_field):
     char_field.save()
     return char_field
 
+
 @pytest.fixture
-def form_class(char_field, int_field):
-    return get_form_class([char_field, int_field])
+def form_class(char_field):
+    return get_form_class((char_field,))
 
 
 def test_form_validates(form_class):
@@ -37,11 +39,10 @@ def test_form_do_not_validates(form_class):
     assert form.is_valid()
 
 
-
 class TestField:
     def test_create_correct_form_field(self, field):
         field = get_form_field(field)
-        assert type(field) is form.CharField
+        assert type(field) is field.CharField
 
 
 class TestFieldValue:

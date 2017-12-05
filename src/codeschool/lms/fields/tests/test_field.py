8e2ca6852f68956132bb45c5180b15153f356edc
ArthurTemporim <arthurrtl@gmail.com>
2017-12-05 17:04:12 -0200

    Update TestField class.
    Signed-off-by: ArthurTemporim <arthurrtl@gmail.com>
    Signed-off-by: Vinícius Oliveira <vinifladf@gmail.com>

--- src/codeschool/lms/fields/tests/test_field.py
+++ src/codeschool/lms/fields/tests/test_field.py
@@ -51,68 +51,7 @@ def test_form_do_not_validates(form_class):
 
     form = form_class({'github': 'https://github.com/someone', 'age': 'bad'})
     assert not form.is_valid()
-
-# Finalize test.
-# class TestField:
-#     def test_create_correct_form_field(self, field):
-#         field = get_form_field(field)
-#         assert type(field) is field.CharField
-
-
-def setUp(self):
-        self.field_attributes = {
-            'name': 'GitHub',
-            'field_type': Field.TYPE_URL,
-            'description': 'GitHub'
-        }
-
-        self.serializer_data = {
-            'name': 'URI',
-            'field_type': Field.TYPE_INT,
-            'description': 'URI'
-        }
-
-        self.field = Field.objects.create(**self.field_attributes)
-        self.serializer = FieldSerializer(instance=self.field)
-
-
-@pytest.fixture
-def test_field_serializer(validate):
-    data = validate.serializer.data
-
-    assert (set(data.keys()) == set(['content', 'field', 'user']))
-
-
-@pytest.fixture
-def test_name_field_content(self):
-    data = self.serializer.data
-
-    assert (data['name'] == self.field_attributes['name'])
-
-
-@pytest.fixture
-def test_size_lower_bound(self):
-    self.serializer_data['field_type'] = Field.TYPE_INT
-
-    serializer = FieldValueSerializer(data=self.serializer_data)
-
-    assert not (serializer.is_valid())
-    assert (set(serializer.errors) == set(['field_type']))
-
+    
 
 class TestFieldValue:
-    def create_field_value(self, user):
-        field_value = self.value()
-
-        assert self.content == field_value.content
-        assert self.field == field_value.fields
-        assert self.user == field_value.user
-
-    def field_value(self):
-        return FiledValue(content=self.content, fields=self.field, user=self.student)
-
-    def test_field_value_db_insertion(self):
-        pass
-
-    def teardown_class(cls):
-        pass
+    pass
